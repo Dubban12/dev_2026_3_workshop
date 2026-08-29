ebc20497fb62bbe5559b65e983079585a0d3ebfc
Dubban <dapereira57ucatolica.edu.co>
2026-08-29 17:31:58 -0500

    Formulas de discriminante,raices_cuadraticas,imc,hipotenusa_pitagoras hechas. Test de formulas completado

--- src/formulas/formulas.py
+++ src/formulas/formulas.py
@@ -217,8 +217,9 @@ class Formulas:
         Ejemplo:
             discriminante(1, -3, 2) -> 1
         """
-        pass
-
+        discriminante = b**2 - 4 * a * c
+        return discriminante
+    
     def raices_cuadraticas(self, a, b, c):
         """
         Calcula las raíces reales de una ecuación cuadrática ax^2 + bx + c = 0
@@ -237,7 +238,12 @@ class Formulas:
         Ejemplo:
             raices_cuadraticas(1, -3, 2) -> (2.0, 1.0)
         """
-        pass
+        raiz = self.discriminante(a, b, c)
+        if raiz < 0:
+            raise ValueError("La ecuación no tiene raíces reales.")
+        raiz1 = (-b + raiz**0.5) / (2 * a)
+        raiz2 = (-b - raiz**0.5) / (2 * a)
+        return (raiz1, raiz2)
 
     def imc(self, peso, altura):
         """
@@ -255,7 +261,8 @@ class Formulas:
         Ejemplo:
             imc(70, 1.75) -> 22.86
         """
-        pass
+        imc = peso / (altura ** 2)
+        return imc
 
     def hipotenusa_pitagoras(self, cateto1, cateto2):
         """
@@ -273,4 +280,5 @@ class Formulas:
         Ejemplo:
             hipotenusa_pitagoras(3, 4) -> 5.0
         """
-        pass
+        h = (cateto1 ** 2 + cateto2 ** 2) ** 0.5
+        return h
