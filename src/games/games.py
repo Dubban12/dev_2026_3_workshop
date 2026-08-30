f4a42476ebf2bbe92622e500d2bd18c9a68c4fe0
Dubban <dapereira57ucatolica.edu.co>
2026-08-29 21:25:20 -0500

    Funcion de ta_te_ti_ganador hecha

--- src/games/games.py
+++ src/games/games.py
@@ -70,7 +70,18 @@ class Games:
              ["O", "O", " "],
              [" ", " ", " "]] -> "X"
         """
-        pass
+        for fila in tablero:
+            if fila[0] == fila[1] == fila[2] != " ":
+                return fila[0]
+        for col in range(3):
+            if tablero[0][col] == tablero[1][col] == tablero[2][col] != " ":
+                return tablero[0][col]
+        if tablero[0][0] == tablero[1][1] == tablero[2][2] != " ":
+            return tablero[0][0]
+        for fila in tablero
+            if " " in fila:
+                return "continua"
+        return "empate"
     
     def generar_combinacion_mastermind(self, longitud, colores_disponibles):
         """
